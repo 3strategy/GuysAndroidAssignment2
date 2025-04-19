64963ebeef258c24ef8140cbc58abaa5b5722b4c
Guy Siedes <3strategy@gmail.com>
2025-04-19 10:44:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/guysassignment/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.guysassignment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.guysassignment.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // added data initializaion for testing:
+        // Since data persists using androids SharedPreferences this initialization code only needs to be run once.
+        // After that, lines should be commneted and data will be fetched from the SharedPreferences .
+        //        SharedViewModel vm = new ViewModelProvider(this).get(SharedViewModel.class);
+        //        vm.setName("Guy");
+        //        vm.setFamilyName("Siedes");
+        //        vm.setBestScore(42);
+
+    }
+
+}
